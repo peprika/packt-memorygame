5ff30c30c7e3052570e451f06e43b3b680c083b3
Riku Pepponen <riku.pepponen@gmail.com>
2017-06-20 16:23:29 +0300

    Set up New Game Button

--- app/src/main/java/com/packtpub/packt_memorygame/MainActivity.java
+++ app/src/main/java/com/packtpub/packt_memorygame/MainActivity.java
@@ -1,13 +1,27 @@
 package com.packtpub.packt_memorygame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button mNewGameButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mNewGameButton = (Button)findViewById(R.id.newGameButton);
+        mNewGameButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(this, GameActivity.class);
+        startActivity(i);
     }
 }
