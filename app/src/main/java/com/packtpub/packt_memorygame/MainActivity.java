16f26552cc2ca780156a40f66bd1ef333a9b03e9
Riku Pepponen <riku.pepponen@gmail.com>
2017-08-25 15:51:22 +0300

    Declarations for high scores

--- app/src/main/java/com/packtpub/packt_memorygame/MainActivity.java
+++ app/src/main/java/com/packtpub/packt_memorygame/MainActivity.java
@@ -1,14 +1,23 @@
 package com.packtpub.packt_memorygame;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button mNewGameButton;
+
+    // Declarations for high scores
+    SharedPreferences prefs;
+    String dataName = "MyData";
+    String intName = "Mystring";
+    int defaultInt = 0;
+    public static int hiScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
